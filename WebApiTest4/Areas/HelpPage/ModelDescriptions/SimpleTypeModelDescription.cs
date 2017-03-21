4e25ff5b30f87455c134b6666322339cb94d9af7
Oleg Shatin 11-501 <mongoose0911@gmail.com>
2017-03-21 17:19:27 +0300

    Добавьте файлы проекта.

---
+++ WebApiTest4/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace WebApiTest4.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ WebApiTest4/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(WebApiTest4.Startup))]
+
+namespace WebApiTest4
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
